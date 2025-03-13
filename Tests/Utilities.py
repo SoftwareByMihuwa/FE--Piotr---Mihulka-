d9a9c602f16afa3c33088f83b749d8939a100063
unknown <piotr.zajebisty@gmail.com>
2025-03-13 23:56:18 +0100

    Adding more chrome options

--- Tests/Utilities.py
+++ Tests/Utilities.py
@@ -5,4 +5,9 @@ def edit_chrome_options():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     return chrome_options
