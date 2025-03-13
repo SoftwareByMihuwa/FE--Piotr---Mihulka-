d3d5534b5677128011f663b44c6bf76101a7fd3b
unknown <piotr.zajebisty@gmail.com>
2025-03-13 23:48:40 +0100

    Updating browser settings

--- Tests/Utilities.py
+++ Tests/Utilities.py
@@ -3,5 +3,6 @@ from selenium.webdriver.chrome.options import Options
 
 def edit_chrome_options():
     chrome_options = Options()
-    chrome_options.add_argument('--start-maximized')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--window-size=1920,1080')
     return chrome_options
