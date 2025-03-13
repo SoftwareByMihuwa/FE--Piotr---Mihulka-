6f821994ff1e1aded113fb0c51e55192475ae610
unknown <piotr.zajebisty@gmail.com>
2025-03-14 00:46:19 +0100

    Trying a JavaScript solution for ExitIntent in headless mode

--- Tests/Utilities.py
+++ Tests/Utilities.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 def edit_chrome_options():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('--window-size=1920,1080')
     # chrome_options.add_argument('--disable-gpu')
     # chrome_options.add_argument('--ignore-certificate-errors')
     # chrome_options.add_argument('--disable-extensions')
--- Tests/test_ExitIntent.py
+++ Tests/test_ExitIntent.py
@@ -1,5 +1,4 @@
 import pytest
-import pyautogui
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -15,19 +14,31 @@ def test_exit_intent():
     try:
         # Navigate to exit intent page
         driver.get('https://the-internet.herokuapp.com/exit_intent')
-        # Move mouse outside of the viewport
-        pyautogui.moveTo(2000, 0)
-        # Waiting a second to make that the window is visible
+        
+        # Trigger exit intent using JavaScript
+        driver.execute_script("""
+            var event = new MouseEvent('mouseout', {
+                'view': window,
+                'bubbles': true,
+                'cancelable': true,
+                'clientX': 0,
+                'clientY': -100
+            });
+            document.dispatchEvent(event);
+        """)
+        
+        # Wait for modal to appear
         wait = WebDriverWait(driver, 10)
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.modal-footer")))
+        
         # Click on the exit intent
         driver.find_element(By.CSS_SELECTOR, "div.modal-footer").click()
+        
         # Verify the exit intent is closed
         assert driver.find_element(By.CSS_SELECTOR, "div.modal-footer").is_displayed() == False
         
     finally:
         print("Test completed")
-    
 
 # This line of code is used to run the test when the file is executed directly
 if __name__ == "__main__":
